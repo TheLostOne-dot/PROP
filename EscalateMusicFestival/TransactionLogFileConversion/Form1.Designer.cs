b01e7e016096da1994b4a2ce18d8023b506305fd
Ignas <412887@student.fontys.nl>
2019-06-04 14:33:05 +0200

    design for transactionlog

--- EscalateMusicFestival/TransactionLogFileConversion/Form1.Designer.cs
+++ EscalateMusicFestival/TransactionLogFileConversion/Form1.Designer.cs
@@ -29,12 +29,125 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnStartLog = new System.Windows.Forms.Button();
+            this.lbxLogs = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnManual = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStartLog
+            // 
+            this.btnStartLog.BackColor = System.Drawing.Color.Black;
+            this.btnStartLog.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStartLog.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartLog.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStartLog.Location = new System.Drawing.Point(12, 137);
+            this.btnStartLog.Name = "btnStartLog";
+            this.btnStartLog.Size = new System.Drawing.Size(144, 23);
+            this.btnStartLog.TabIndex = 0;
+            this.btnStartLog.Text = "Start ";
+            this.btnStartLog.UseVisualStyleBackColor = false;
+            this.btnStartLog.Click += new System.EventHandler(this.btnStartLog_Click);
+            // 
+            // lbxLogs
+            // 
+            this.lbxLogs.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxLogs.FormattingEnabled = true;
+            this.lbxLogs.ItemHeight = 18;
+            this.lbxLogs.Location = new System.Drawing.Point(173, 12);
+            this.lbxLogs.Name = "lbxLogs";
+            this.lbxLogs.Size = new System.Drawing.Size(186, 436);
+            this.lbxLogs.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TransactionLogFileConversion.Properties.Resources.WhatsApp_Image_2019_05_28_at_15_17_48;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 119);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 29;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnManual
+            // 
+            this.btnManual.BackColor = System.Drawing.Color.Black;
+            this.btnManual.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnManual.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManual.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnManual.Location = new System.Drawing.Point(12, 166);
+            this.btnManual.Name = "btnManual";
+            this.btnManual.Size = new System.Drawing.Size(144, 23);
+            this.btnManual.TabIndex = 30;
+            this.btnManual.Text = "Manual";
+            this.btnManual.UseVisualStyleBackColor = false;
+            this.btnManual.Click += new System.EventHandler(this.btnManual_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Black;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBack.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnBack.Location = new System.Drawing.Point(2, 387);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(142, 45);
+            this.btnBack.TabIndex = 31;
+            this.btnBack.Text = "⮘  BACK";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(162, -16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 482);
+            this.panel1.TabIndex = 32;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(-120, 371);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(569, 5);
+            this.panel2.TabIndex = 33;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(364, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnManual);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbxLogs);
+            this.Controls.Add(this.btnStartLog);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStartLog;
+        private System.Windows.Forms.ListBox lbxLogs;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnManual;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
