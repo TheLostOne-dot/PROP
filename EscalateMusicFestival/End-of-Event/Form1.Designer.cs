0f35be0928c3c48c8c999f00932919540238f4ad
Bučinskas,Vilius V <v.bucinskas@student.fontys.nl>
2019-06-04 15:00:03 +0200

    adding end of event app

--- EscalateMusicFestival/End-of-Event/Form1.Designer.cs
+++ EscalateMusicFestival/End-of-Event/Form1.Designer.cs
@@ -32,116 +32,222 @@
             this.btnOpen = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.lbxIDscan = new System.Windows.Forms.ListBox();
-            this.btnMakeInvalid = new System.Windows.Forms.Button();
-            this.btnShowloaneditems = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.lbxLoaneditems = new System.Windows.Forms.ListBox();
-            this.lblBalance = new System.Windows.Forms.Label();
+            this.labelBalance = new System.Windows.Forms.Label();
+            this.lbBalance = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(102, 117);
+            this.btnClose.BackColor = System.Drawing.Color.Black;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnClose.Location = new System.Drawing.Point(12, 216);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(157, 44);
+            this.btnClose.Size = new System.Drawing.Size(187, 36);
             this.btnClose.TabIndex = 21;
-            this.btnClose.Text = "Close Connection";
-            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Text = "CLOSE CONNECTION";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(102, 45);
+            this.btnOpen.BackColor = System.Drawing.Color.Black;
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOpen.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnOpen.Location = new System.Drawing.Point(12, 174);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(2);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(157, 47);
+            this.btnOpen.Size = new System.Drawing.Size(187, 38);
             this.btnOpen.TabIndex = 20;
-            this.btnOpen.Text = "Open Connection";
-            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Text = "OPEN CONNECTION";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.lbxIDscan);
-            this.groupBox1.Location = new System.Drawing.Point(338, 45);
+            this.groupBox1.Location = new System.Drawing.Point(220, 45);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(341, 116);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(346, 114);
             this.groupBox1.TabIndex = 22;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "ID";
             // 
             // lbxIDscan
             // 
+            this.lbxIDscan.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbxIDscan.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbxIDscan.FormattingEnabled = true;
-            this.lbxIDscan.ItemHeight = 16;
-            this.lbxIDscan.Location = new System.Drawing.Point(7, 36);
-            this.lbxIDscan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lbxIDscan.ItemHeight = 18;
+            this.lbxIDscan.Location = new System.Drawing.Point(5, 19);
+            this.lbxIDscan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.lbxIDscan.Name = "lbxIDscan";
-            this.lbxIDscan.Size = new System.Drawing.Size(327, 52);
+            this.lbxIDscan.Size = new System.Drawing.Size(328, 72);
             this.lbxIDscan.TabIndex = 6;
-            // 
-            // btnMakeInvalid
-            // 
-            this.btnMakeInvalid.Location = new System.Drawing.Point(455, 396);
-            this.btnMakeInvalid.Name = "btnMakeInvalid";
-            this.btnMakeInvalid.Size = new System.Drawing.Size(224, 42);
-            this.btnMakeInvalid.TabIndex = 23;
-            this.btnMakeInvalid.Text = "Mark as Invalid";
-            this.btnMakeInvalid.UseVisualStyleBackColor = true;
-            // 
-            // btnShowloaneditems
-            // 
-            this.btnShowloaneditems.Location = new System.Drawing.Point(464, 196);
-            this.btnShowloaneditems.Name = "btnShowloaneditems";
-            this.btnShowloaneditems.Size = new System.Drawing.Size(185, 45);
-            this.btnShowloaneditems.TabIndex = 24;
-            this.btnShowloaneditems.Text = "Show Loaned Items";
-            this.btnShowloaneditems.UseVisualStyleBackColor = true;
-            this.btnShowloaneditems.Click += new System.EventHandler(this.btnShowloaneditems_Click);
             // 
             // groupBox2
             // 
+            this.groupBox2.BackColor = System.Drawing.Color.White;
             this.groupBox2.Controls.Add(this.lbxLoaneditems);
-            this.groupBox2.Location = new System.Drawing.Point(53, 196);
+            this.groupBox2.Location = new System.Drawing.Point(215, 223);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(386, 217);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(346, 193);
             this.groupBox2.TabIndex = 25;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Visible = false;
             // 
             // lbxLoaneditems
             // 
+            this.lbxLoaneditems.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbxLoaneditems.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbxLoaneditems.FormattingEnabled = true;
-            this.lbxLoaneditems.ItemHeight = 16;
-            this.lbxLoaneditems.Location = new System.Drawing.Point(6, 21);
+            this.lbxLoaneditems.ItemHeight = 18;
+            this.lbxLoaneditems.Location = new System.Drawing.Point(4, 17);
+            this.lbxLoaneditems.Margin = new System.Windows.Forms.Padding(2);
             this.lbxLoaneditems.Name = "lbxLoaneditems";
-            this.lbxLoaneditems.Size = new System.Drawing.Size(367, 180);
+            this.lbxLoaneditems.Size = new System.Drawing.Size(333, 162);
             this.lbxLoaneditems.TabIndex = 26;
             // 
-            // lblBalance
+            // labelBalance
             // 
-            this.lblBalance.AutoSize = true;
-            this.lblBalance.Location = new System.Drawing.Point(335, 164);
-            this.lblBalance.Name = "lblBalance";
-            this.lblBalance.Size = new System.Drawing.Size(102, 17);
-            this.lblBalance.TabIndex = 26;
-            this.lblBalance.Text = "Visitor Balance";
+            this.labelBalance.AutoSize = true;
+            this.labelBalance.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBalance.Location = new System.Drawing.Point(222, 161);
+            this.labelBalance.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelBalance.Name = "labelBalance";
+            this.labelBalance.Size = new System.Drawing.Size(133, 18);
+            this.labelBalance.TabIndex = 26;
+            this.labelBalance.Text = "VISITOR BALANCE";
+            this.labelBalance.Click += new System.EventHandler(this.labelBalance_Click);
+            // 
+            // lbBalance
+            // 
+            this.lbBalance.AutoSize = true;
+            this.lbBalance.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBalance.Location = new System.Drawing.Point(360, 161);
+            this.lbBalance.Name = "lbBalance";
+            this.lbBalance.Size = new System.Drawing.Size(0, 18);
+            this.lbBalance.TabIndex = 27;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::End_of_Event.Properties.Resources.Resized;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 157);
+            this.pictureBox1.TabIndex = 28;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(205, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 482);
+            this.panel1.TabIndex = 29;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Black;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBack.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnBack.Location = new System.Drawing.Point(12, 432);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(187, 54);
+            this.btnBack.TabIndex = 30;
+            this.btnBack.Text = "⮘  BACK";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(7, 421);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(569, 5);
+            this.panel2.TabIndex = 30;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(216, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 23);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "LOANED ITEMS:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(221, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 23);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "ID:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Enabled = false;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(215, 431);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(346, 55);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "MARK AS INVALID";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnMakeInvalid_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblBalance);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(581, 497);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbBalance);
+            this.Controls.Add(this.labelBalance);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.btnShowloaneditems);
-            this.Controls.Add(this.btnMakeInvalid);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "End-of-Event";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,11 +259,17 @@
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ListBox lbxIDscan;
-        private System.Windows.Forms.Button btnMakeInvalid;
-        private System.Windows.Forms.Button btnShowloaneditems;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.ListBox lbxLoaneditems;
-        private System.Windows.Forms.Label lblBalance;
+        private System.Windows.Forms.Label labelBalance;
+        private System.Windows.Forms.Label lbBalance;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- EscalateMusicFestival/End-of-Event/Form1.cs
+++ EscalateMusicFestival/End-of-Event/Form1.cs
@@ -7,27 +7,145 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Phidget22;
+using Phidget22.Events;
 
 namespace End_of_Event
 {
     public partial class Form1 : Form
     {
+        private string tag;
+        private RFID myRFIDReader;
+        private DataHelper dh;
+        
         public Form1()
         {
             InitializeComponent();
+            tag = "";
+            myRFIDReader = new RFID();
+            dh = new DataHelper();
+            myRFIDReader.Attach += MyRFID_Attached;
+            myRFIDReader.Detach += MyRFID_Detached;
+            myRFIDReader.Tag += MyRFID_Taged;
         }
-
-        private void btnShowloaneditems_Click(object sender, EventArgs e)
+        public void MyRFID_Attached(object sender, AttachEventArgs args)
         {
-            if (lbxLoaneditems == null)
+            lbxIDscan.Items.Add("RFID Reader attached");
+        }
+        public void MyRFID_Detached(object sender, DetachEventArgs args)
+        {
+            lbxIDscan.Items.Add("RFID Reader detached");
+        }
+        public void MyRFID_Taged(object sender, RFIDTagEventArgs args)
+        {
+            lbxIDscan.Items.Add($"RFID Taged! ID: {args.Tag}");
+            tag = args.Tag.ToString();
+            double balance = dh.GetBalance(args.Tag);
+            if (balance > -1)
             {
-                MessageBox.Show("No Loaned Items");
+                lbBalance.Text = balance.ToString() + "€";
             }
             else
             {
-                lbxLoaneditems.Visible = true;
+                lbBalance.Text = "0€";
             }
+            List<List<string>> inf = dh.Items(args.Tag);
+            int i = 0;
+            if (!inf.Any())
+            {
+                
+                button1.Enabled = true;
+                
+            }
+            else
+            {
+                
+                button1.Enabled = false;
+                foreach (var g in inf)
+                {
+                    lbxLoaneditems.Items.Add($"Item: {g[0]} with Price of {g[1]}€ from Stand: {g[2]} is STILL LOANED");
+                    i++;
+                }
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
             
+        }
+
+
+        private void btnShowloaneditems_Click(object sender, EventArgs e)
+        {
+            //if (lbxLoaneditems == null)
+            //{
+            //    MessageBox.Show("No Loaned Items");
+            //}
+            //else
+            //{
+            //    lbxLoaneditems.Visible = true;
+            //}
+            
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myRFIDReader.Open();
+                lbxIDscan.Items.Add("Connection opened");
+
+            }
+            catch (PhidgetException)
+            {
+                lbxIDscan.Items.Clear();
+                lbxIDscan.Items.Add("Connection failed:Please try again");
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            myRFIDReader.Close();
+            lbxIDscan.Items.Add("Connection closed");
+        }
+
+        private void btnMakeInvalid_Click(object sender, EventArgs e)
+        {
+            if (tag != "")
+            {
+                int valid = dh.Invalid(tag);
+                if (valid > -1)
+                {
+                    lbxIDscan.Items.Add($"{tag} Marked as Invalid");
+                }
+                else
+                {
+                    MessageBox.Show("Something went wrong");
+                }
+            }
+        }
+
+        private void labelBalance_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            int l = Application.StartupPath.ToString().Length;
+            System.Diagnostics.Process.Start(Application.StartupPath.ToString().Remove(l - 22) + @"\EscalateMusicFestival\bin\Debug\EscalateMusicFestival.exe");
+            this.Close();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
